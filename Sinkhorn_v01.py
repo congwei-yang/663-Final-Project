52addc423bccd1c2fdc3f184a46eafbcb2c77522
congwei-yang <congwei.yang@duke.edu>
2021-04-14 14:52:50 -0400

    Sinkhorn numba version

--- Sinkhorn_v01.py
+++ Sinkhorn_v01.py
@@ -2,14 +2,14 @@ import numpy as np
 import idx2numpy
 from numba import jit
 @jit(nopython=True)
-def Sinkhorn(r, C, M, lamda, tol = 1e-7):
+def Sinkhorn(r, C, M, lamda, maxiter = 10000, tol = 1e-7):
     M = M[r > 0]
     r = r[r > 0]
     K = np.exp(-lamda * M)
     N = np.shape(C)[1]
     u = np.ones((len(r), N)) / len(r)
     K_tilde = np.diag(1/r) @ K
-    d_prev = np.repeat(2, N) + 0.5
+    d_prev = np.repeat(2., N)
     d = np.ones(N) + 0.5
     niter = 0
     while np.max(np.abs(d - d_prev)) > tol:
@@ -18,4 +18,6 @@ def Sinkhorn(r, C, M, lamda, tol = 1e-7):
         d_prev = d
         d = np.sum(u * ((K * M) @ v), axis = 0)
         niter = niter + 1
+        if niter > maxiter:
+            break
     return d[0], niter
