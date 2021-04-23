6d8656ac7ddaf38fb554a0a0b1b3b0294900fe6f
a <yjzhang19@outlook.com>
2021-04-23 22:35:56 +0800

    Package setup, Test functions.

--- Sinkhorn_663/Sinkhorn_main.py
+++ Sinkhorn_663/Sinkhorn_main.py
@@ -1,5 +1,6 @@
 from .Sinkhorn_v01 import Sinkhorn_numba
 from .log_domain_skh import log_domain_sinkhorn_2
+from Skh_cpp import Sinkhorn_cpp
 import numpy as np
 
 def Sinkhorn(r, C, M, lamda=20, tol=1e-6, maxiter=50, log_domain=False, cpp=False):
@@ -9,11 +10,9 @@ def Sinkhorn(r, C, M, lamda=20, tol=1e-6, maxiter=50, log_domain=False, cpp=Fals
         N = np.shape(C)[1]
         results = np.zeros(N)
         for i in range(N):
-            results[i] = log_domain_sinkhorn_2(r, C[:, i], M, lamda, tol, maxiter)
+            results[i] = log_domain_sinkhorn_2(r, C[:, i], M, lamda, tol, maxiter)[0]
         return results
     elif cpp == True:
-        import cppimport
-        Skh_cpp = cppimport.imp("Skh_cpp")
-        return Skh_cpp.Sinkhorn_cpp(r, C, M, lamda, tol, maxiter)
+        return Sinkhorn_cpp(r, C, M, lamda, tol, maxiter)
     else:
         return Sinkhorn_numba(r, C, M, lamda, tol, maxiter)
