dce96f22dcd89abd947b265c864f15d285bcfc56
a <yjzhang19@outlook.com>
2021-04-23 16:19:04 +0800

    Add setup.py

--- Sinkhorn_663/Sinkhorn_main.py
+++ Sinkhorn_663/Sinkhorn_main.py
@@ -1,5 +1,5 @@
-from Sinkhorn_v01 import Sinkhorn_numba
-from log_domain_skh import log_domain_sinkhorn_2
+from .Sinkhorn_v01 import Sinkhorn_numba
+from .log_domain_skh import log_domain_sinkhorn_2
 import numpy as np
 
 def Sinkhorn(r, C, M, lamda=20, tol=1e-6, maxiter=50, log_domain=False, cpp=False):
@@ -17,4 +17,3 @@ def Sinkhorn(r, C, M, lamda=20, tol=1e-6, maxiter=50, log_domain=False, cpp=Fals
         return Skh_cpp.Sinkhorn_cpp(r, C, M, lamda, tol, maxiter)
     else:
         return Sinkhorn_numba(r, C, M, lamda, tol, maxiter)
-
