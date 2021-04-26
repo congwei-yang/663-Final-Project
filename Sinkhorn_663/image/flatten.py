4556a6b81f3c8beb5a94768ffa57dc92964d11f4
HaoliangZheng <2016310868@email.cufe.edu.cn>
2021-04-26 04:51:18 -0400

    a new image folder one data one notebook

---
+++ Sinkhorn_663/image/flatten.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def flatten(data):
+    """
+    Flatten a list of image data from matrices to vectors.
+    Args
+        data: a list of image matrices
+    Returns
+        a list of image vectors, which can be put into sinkhorn functions.
+    """
+    data_flat = []
+    for i in range(0, len(data)):
+        K = data[i]
+        K_normal = K / sum(np.matrix.flatten(K, order='F'))
+        K_flat = np.matrix.flatten(K_normal, order='F')
+        data_flat.append(K_flat)
+    return data_flat
